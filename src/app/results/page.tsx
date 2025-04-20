446bfc8cdb642777c3c5d4e65dd703a46caacdba
codechefvitcc <86666331+codechefvitcc@users.noreply.github.com>
2025-04-20 14:27:42 +0530

    Update page.tsx

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ResultsPage = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center text-5xl font-bold text-gray-800'>
-        You really thought we'll show the results to you???? 😂
+        Did you really think we would show you the results????? 😂
     </div>
   )
 }
